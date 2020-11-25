d9cee2a3537461984d27d3bf68c77f4d69f8f695
Angel-J-CO <62389274+Angel-CO@users.noreply.github.com>
2020-11-24 21:38:58 -0600

    Update Servicios.cs

--- Memorama-Host/Contratos/Servicios.cs
+++ Memorama-Host/Contratos/Servicios.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
@@ -6,16 +6,32 @@ using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace Contratos
 {
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Single)]
     public class Servicios :IContratos
     {
-        private Dictionary<IContratosCallBack, string> usuariosConectados = new Dictionary<IContratosCallBack, string>();
-        private List<string> usuariosMensaje = new List<string>();
         public void Login(Usuario usuario)
         {
+            using (MemoramaDBEntities db = new MemoramaDBEntities())
+            {
+                var usu = db.Usuario.Where((x) => x.Nickname == usuario.Nickname).FirstOrDefault();
+                if (usu != null)
+                {
+                    if (usu.Password.Equals(usuario.Password))
+                    {
+                        Callback.GetLoginResult(LoginResults.UsuarioEncontrado);
+                    }
+                    else
+                    {
+                        Callback.GetLoginResult(LoginResults.ContraseñaIncorrecta);
+                    }
+                }
+                else
+                {
+                    Callback.GetLoginResult(LoginResults.NoExisteUrsuario);
+                }
+            }
             
         }
 
